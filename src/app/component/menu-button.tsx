e81940e82c086300bd85b0648097fa7af1de79d0
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-07-08 17:32:53 +0900

    【修正】menu-typographyからmenu-buttonへ

---
+++ src/app/component/menu-button.tsx
@@ -0,0 +1,30 @@
+import { Link, Typography } from "@mui/material";
+import "./style.css";
+type MenuTypographyProps = {
+  title: string;
+  color: string;
+  href: string;
+};
+
+const MenuButton: React.FC<MenuTypographyProps> = ({ title, color, href }) => {
+  return (
+    <Link
+      href={href}
+      underline="hover"
+      color={color}
+      fontSize={20}
+      mx={1}
+      sx={{
+        textDecorationColor: { color },
+        fontFamily: "Jomolhari",
+        fontWeight: 400,
+        fontStyle: "normal",
+        textTransform: "capitalize",
+      }}
+    >
+      {title}
+    </Link>
+  );
+};
+
+export default MenuButton;
