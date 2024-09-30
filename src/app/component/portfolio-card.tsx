2e531733efdc6a91943c7734c42ec29b79beccc5
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-10-01 00:46:44 +0900

    rename: show-portfolioをportfolio-cardに変更

---
+++ src/app/component/portfolio-card.tsx
@@ -0,0 +1,33 @@
+import { Box, Typography, Link } from "@mui/material";
+
+type PortfolioCardProps = {
+  image: string;
+  title: string;
+  href: string;
+};
+
+const PortfolioCard: React.FC<PortfolioCardProps> = ({
+  image,
+  title,
+  href,
+}) => {
+  return (
+    <Box>
+      <Link href={href}>
+        <Box boxShadow={2}>
+          <img src={image} alt={"作品イメージ"} width={504} height={314} />
+        </Box>
+      </Link>
+      <Link
+        href={href}
+        underline="hover"
+        color={"#857076"}
+        sx={{ textDecorationColor: "#857076" }}
+      >
+        <Typography>{title}</Typography>
+      </Link>
+    </Box>
+  );
+};
+
+export default PortfolioCard;
